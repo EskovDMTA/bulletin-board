9d32a8dcd47cc9213b471b362664550e8e91d10e
eskovdmt <DmtEskov@gmail.com>
2023-12-03 22:02:14 +0300

    feat: delete admin and api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,9 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  namespace 'api' do
-    resources :category
-  end
-
-  namespace 'admin' do
-    resource :profile
-  end
-
   scope module: 'web' do
     root 'home_page#home'
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
-    resources :category
   end
 end
